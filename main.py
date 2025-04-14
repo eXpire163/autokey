d74717378b719d7876f317b5d36d0bef321acb8a
eXpire163 <github@mms2k.de>
2025-04-14 19:44:30 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,77 +1,3 @@
-
-# import keyboard
-# import time
-# import threading
-# import random
-# import pyautogui
-# import os
-
-# def press_key(key, interval):
-#     while True:
-#         if not paused:
-#             keyboard.press_and_release(str(key))
-#         time.sleep(interval + random.uniform(0, 2.2))
-
-# def click_mouse(interval, key):
-#     active_clicks[key] = True
-#     while active_clicks[key]:
-#         if not paused:
-#             x, y = pyautogui.position()
-#             offset_x = random.randint(-5, 5)
-#             offset_y = random.randint(-5, 5)
-#             pyautogui.click(x + offset_x, y + offset_y)
-#         time.sleep(interval + random.uniform(0, 2.2))
-#     active_clicks[key] = False
-
-# def toggle_pause():
-#     global paused
-#     paused = not paused
-#     print("Paused" if paused else "Resumed")
-#     update_display()
-
-# def toggle_clicking(key, interval):
-#     if key in active_clicks and active_clicks[key]:
-#         active_clicks[key] = False
-#     else:
-#         threading.Thread(target=click_mouse, args=(interval, key), daemon=True).start()
-#     update_display()
-
-# def update_display():
-#     os.system('cls' if os.name == 'nt' else 'clear')
-#     print("=== Script Status ===")
-#     print(f"Paused: {'Yes' if paused else 'No'}")
-#     print("Active Clicks:")
-#     for key, active in active_clicks.items():
-#         print(f" - {key.upper()}: {'Active' if active else 'Inactive'}")
-#     print("\nPress 'P' to pause/resume. Press 'ESC' to exit.")
-
-# time.sleep(3)
-
-# paused = False
-# active_clicks = {"s": False, "f": False}
-# keyboard.add_hotkey('p', toggle_pause)
-
-# # Start/stop mouse click threads on 's' and 'f' key press
-# keyboard.add_hotkey('s', lambda: toggle_clicking("s", 60))
-# keyboard.add_hotkey('f', lambda: toggle_clicking("f", 5))
-
-# keys_intervals = {1: 4, 2: 15, 3: 20, 4: 15, 5: 30}
-# threads = []
-
-# for key, interval in keys_intervals.items():
-#     thread = threading.Thread(target=press_key, args=(key, interval), daemon=True)
-#     thread.start()
-#     threads.append(thread)
-
-
-
-# update_display()
-# keyboard.wait('esc')
-
-
-
-
-
 import keyboard
 import time
 import threading
@@ -80,6 +6,11 @@ import pyautogui
 import os
 import curses
 # pip install windows-curses
+# pip install pyautogui
+# pip install keyboard
+
+def randomi(i):
+    return random.randint(-i, i)    
 
 def press_key(key, interval):
     while True:
@@ -96,10 +27,8 @@ def countdown(row, interval):
 def click_mouse_fast(key, interval):
     row = 2
     while True:
-        if mouse_fast:            
-            offset_x = random.randint(-5, 5)
-            offset_y = random.randint(-5, 5)
-            pyautogui.click(x + offset_x, y + offset_y)
+        if mouse_fast:           
+            pyautogui.click(x + randomi(5), y + randomi(5))
             countdown(row, fast_countdown)          
         else:
             update_countdown(row, 0)
@@ -109,9 +38,7 @@ def click_mouse_slow(key, interval):
     row = 3
     while True:
         if mouse_slow:            
-            offset_x = random.randint(-2, 2)
-            offset_y = random.randint(-2, 2)
-            pyautogui.click(x + offset_x, y + offset_y)
+            pyautogui.click(x + randomi(2), y + randomi(2))
             countdown(row, slow_countdown)
         else:
             update_countdown(row, 0)
@@ -123,83 +50,78 @@ def click_mouse_plants(key, interval):
     row = 4
     while True:
         if mouse_plants:            
-            offset_x = random.randint(-2, 2)
-            offset_y = random.randint(-2, 2)
-            pyautogui.click(x + offset_x, y + offset_y)
-            plants_action = "growing"
+            pyautogui.click(x + randomi(2), y + randomi(2))
+            update_countdown(5,"growing")
             countdown(row, plan_growing_countdown)
-            offset_x = random.randint(-2, 2)
-            offset_y = random.randint(-2, 2)
-            pyautogui.click(x + offset_x, y + offset_y)
-            plants_action = "harvesting "
+            pyautogui.click(x + randomi(2), y + randomi(2))
+            update_countdown(5,"harvesting")
             countdown(row, plan_harvesting_countdown)
         else:
             update_countdown(row, 0)
-            plants_action = "nothing"
+            update_countdown(5, "nothing")
             time.sleep(pause_time)
 
-# def update_display():
-#     os.system('cls' if os.name == 'nt' else 'clear')
-#     print("=== Active Components ===")
-#     print(f"(K)eys:\t\t{'Yes' if keys_active else 'No'}")
-#     print(f"(F)ast mouse:\t{'Yes' if mouse_fast else 'No'}")
-#     print(f"(S)low mouse:\t{'Yes' if mouse_slow else 'No'}")
-#     print(f"(P)lant mouse:\t{'Yes' if mouse_plants else 'No'}")
-#     print(f"(P)lant:\t{plants_action}")
-#     print(f"Pos: \t\t{x},{y}")
 
-#     print("\nPress 'ESC' to exit.")
-
-
-def update_display():
+def setup_display():
     stdscr.addstr(0, 0, "=== Active Components ===")
     stdscr.addstr(1, 0, f"(K)eys:\t\t{'Yes' if keys_active else 'No '}")
     stdscr.addstr(2, 0, f"(F)ast mouse:\t{'Yes' if mouse_fast else 'No '}")
     stdscr.addstr(3, 0, f"(S)low mouse:\t{'Yes' if mouse_slow else 'No '}")
     stdscr.addstr(4, 0, f"(P)lant mouse:\t{'Yes' if mouse_plants else 'No '}")
-    stdscr.addstr(5, 0, f"(P)lant:\t{plants_action}")
+    stdscr.addstr(5, 0, f"(P)lant:")
     stdscr.addstr(6, 0, f"Pos: \t\t{x},{y}       ")
     stdscr.addstr(8, 0, "Press 'ESC' to exit.")
     stdscr.refresh()
 
 
 def update_countdown(row, i):
-    stdscr.addstr(row, 30, f"({i})  ")
+    if (isinstance(i, str) or i > 0):
+        stdscr.addstr(row, 23, f"({i})           ")
+    # elif (isinstance(i, str)):
+    #     stdscr.addstr(row, 23, f"({i})  ")
+    else:
+        stdscr.addstr(row, 23, "                     ")
     stdscr.refresh()
 
 def toggle_pause():
     global keys_active
     keys_active = not keys_active
-    update_display()
+    setup_display()
 
 def toggle_mouse_slow():
-    global mouse_fast, mouse_slow,x,y
+    global mouse_plants, mouse_slow, mouse_fast, x, y
     x, y = pyautogui.position()
     mouse_slow = not mouse_slow
     if(mouse_slow):
         mouse_fast = False
-    update_display()
+        mouse_plants = False
+
+    setup_display()
 
 def toggle_mouse_fast():
-    global mouse_fast, mouse_slow,x,y
+    global mouse_plants, mouse_slow, mouse_fast, x, y
     x, y = pyautogui.position()
     mouse_fast = not mouse_fast
     if(mouse_fast):
         mouse_slow = False
-    update_display()
+        mouse_plants = False
+    setup_display()
 
 def toggle_mouse_plants():
-    global mouse_plants,x,y
+    global mouse_plants, mouse_slow, mouse_fast, x, y
     x, y = pyautogui.position()
     mouse_plants = not mouse_plants
-    update_display()
+    if(mouse_plants):
+        mouse_slow = False
+        mouse_fast = False
+    setup_display()
 
 
 keys_active = False
 mouse_slow = False
 mouse_fast = False
 mouse_plants = False
-plants_action = "nothing     "
+
 slow_countdown = 60
 fast_countdown = 10
 plan_growing_countdown = 30
@@ -221,7 +143,7 @@ def main(stdscrr):
     curses.curs_set(0)  # Hide cursor
     stdscr.clear()
     stdscr.nodelay(1)  # Non-blocking input
-    update_display()
+    setup_display()
     keys_intervals = {1: 4, 2: 15, 3: 190, 4: 14, 5: 28}
 
     threads = []
